def760ccacbb1e994ad95e84c0588ad15b3d488f
Jennifer Dobak <jdobak1@gmail.com>
2018-05-09 21:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ function deepestChild() {
 }
 
 function increaseRankBy(n) {
-  let lis = document.querySelectorAll('div.ranked-list li')
-  for (let i = 0; i < lis.length; i++) {
-    // lis[i].innerHTML = paresInt(lis[i].innerHTML) + n
-    lis[i] = paresInt(lis[i].innerHTML) + n
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
 }
