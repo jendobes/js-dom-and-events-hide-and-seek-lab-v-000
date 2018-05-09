63a8f38208eab16b75b1041cc4943cb7ac04f9ab
Jennifer Dobak <jdobak1@gmail.com>
2018-05-09 21:15:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nestedTarget(){
   return document.querySelector('div#nested div.target')
 }
 
-function deepestChild(divs) {
+function deepestChild() {
   let divs = document.querySelectorAll('div#grand-node div')
 
   return divs[divs.length - 1]
