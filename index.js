63a15f83a9ad0749119eef1f3335694d599b2a38
Jennifer Dobak <jdobak1@gmail.com>
2018-05-09 21:22:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,7 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   const lis = document.querySelectorAll('div.ranked-list li')
+  for (let i = 0, i < lis.length; i++) {
+    lis[i].innerHTML = lis[i].innerHTML + 1
+  }
 }
