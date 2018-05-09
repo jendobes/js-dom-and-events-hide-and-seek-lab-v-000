cec8d64cc46d8eefad967d9adce99afb517a6acb
Jennifer Dobak <jdobak1@gmail.com>
2018-05-09 21:53:04 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('div#nested div.target')
+}
+
+function deepestChild() {
+  let divs = document.querySelectorAll('div#grand-node div')
+
+  return divs[divs.length - 1]
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+  }
+}
